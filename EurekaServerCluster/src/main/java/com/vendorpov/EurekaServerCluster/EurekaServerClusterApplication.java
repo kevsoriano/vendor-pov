b7a3f78c5cdfcbf82a38bb51e1b4d611a8051a55
kevsoriano <kevinsoriano173@gmail.com>
2025-11-02 14:06:58 +0800

    add RBAC and jwtAuthorities

---
+++ EurekaServerCluster/src/main/java/com/vendorpov/EurekaServerCluster/EurekaServerClusterApplication.java
@@ -0,0 +1,13 @@
+package com.vendorpov.EurekaServerCluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerClusterApplication.class, args);
+	}
+
+}
