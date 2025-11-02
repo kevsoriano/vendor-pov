1d437d0a06b9208de7ebebd60b3f4a3dcbd5a07f
kevsoriano <kevinsoriano173@gmail.com>
2025-11-02 22:03:53 +0800

    delete old users microservice

--- users/src/main/java/com/vendorpov/users/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.vendorpov.users.data;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
-	UserEntity findByEmail(String email);
-}
